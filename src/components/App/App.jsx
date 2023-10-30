2cf3d50db5a10b7be7b0479152e1b0360fff1f65
Sergey Grigorash <adviser@bk.ru>
2023-10-30 11:59:44 +0500

    feat: make routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Main from "../Main";
+import Register from "../Register";
+import Login from "../Login";
+import Profile from "../Profile";
+import Movies from "../Movies";
+import SavedMovies from "../SavedMovies";
+import Page404 from "../Page404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/signup" element={<Register />} />
+        <Route path="/signin" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/saved-movies" element={<SavedMovies />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
     </div>
   );
 }
