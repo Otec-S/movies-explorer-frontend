02d176c09d60e68ae903d0e7374835a3b29fbac3
Sergey Grigorash <adviser@bk.ru>
2023-10-31 13:11:49 +0500

    feat: make Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import "./Header.css";
 import logoSmile from "../../images/logoSmile.svg";
+import logoMan from "../../images/logMan.jpg";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
-      <nav className="header__nav">
+      <div className="header__nav">
         <div className="header__nav-movies">
-          <img src={logoSmile} className="header__logoSmile" alt="Логотип с улыбкой" />
-          <nav className="header__films">фильмы</nav>
+          <img
+            src={logoSmile}
+            className="header__logoSmile"
+            alt="Логотип с улыбкой"
+          />
+          <nav className="header__films">
+            <Link className="header__link-movie" to="">
+              Фильмы
+            </Link>
+            <Link className="header__link-movie" to="">
+              Сохраненные фильмы
+            </Link>
+          </nav>
         </div>
+
         <div className="header__nav-account">
-          header__nav-account
-          <div className="header__account"></div>
-          <div className="header__logo-man"></div>
+          <Link className="header__link-account" to="">
+            Аккаунт
+          </Link>
+          <img
+            src={logoMan}
+            className="header__logo-man"
+            alt="Логотип с человеком"
+          />
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
