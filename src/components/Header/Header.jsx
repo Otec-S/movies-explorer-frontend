1e11354c341a67c42ced791fd074a88853c2cc68
Sergey Grigorash <adviser@bk.ru>
2023-11-01 11:07:08 +0500

    feat: finish Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,36 +4,56 @@ import logoSmile from "../../images/logoSmile.svg";
 import logoMan from "../../images/logMan.jpg";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ isRegistered }) => {
   return (
     <header className="header">
       <div className="header__nav">
-        <div className="header__nav-movies">
-          <img
-            src={logoSmile}
-            className="header__logoSmile"
-            alt="Логотип с улыбкой"
-          />
-          <nav className="header__films">
-            <Link className="header__link-movie" to="">
-              Фильмы
-            </Link>
-            <Link className="header__link-movie" to="">
-              Сохраненные фильмы
-            </Link>
-          </nav>
-        </div>
+        {isRegistered ? (
+          <>
+            <div className="header__nav-movies">
+              <img
+                src={logoSmile}
+                className="header__logoSmile"
+                alt="Логотип с улыбкой"
+              />
+              <nav className="header__films">
+                <Link className="header__link-movie" to="">
+                  Фильмы
+                </Link>
+                <Link className="header__link-movie" to="">
+                  Сохраненные фильмы
+                </Link>
+              </nav>
+            </div>
+            <div className="header__nav-account">
+              <Link className="header__link-account" to="">
+                Аккаунт
+              </Link>
+              <img
+                src={logoMan}
+                className="header__logo-man"
+                alt="Логотип с человеком"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="header__nav-movies">
+              <img
+                src={logoSmile}
+                className="header__logoSmile"
+                alt="Логотип с улыбкой"
+              />
+            </div>
 
-        <div className="header__nav-account">
-          <Link className="header__link-account" to="">
-            Аккаунт
-          </Link>
-          <img
-            src={logoMan}
-            className="header__logo-man"
-            alt="Логотип с человеком"
-          />
-        </div>
+            <div className="header__nav-account">
+              <Link className="header__link-movie" to="">
+                Регистрация
+              </Link>
+              <button className="header__entrance-button">Войти</button>
+            </div>
+          </>
+        )}
       </div>
     </header>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,7 @@ import Header from '../Header/Header'
 const Main = () => {
   return (
     <div>
-      <Header/>
+      <Header isRegistered={true} />
     </div>
   );
 };
