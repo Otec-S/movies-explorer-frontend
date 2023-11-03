e61e55f73ec9f9c2cdb6af7017069383471b9a0b
Sergey Grigorash <adviser@bk.ru>
2023-11-03 13:07:31 +0500

    feat: doing AboutMe

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -4,16 +4,18 @@ import "./AboutMe.css";
 const AboutMe = () => {
   return (
     <section className="about-me">
-      <div className="about-me__header">Студент</div>
+      <div className="about-me__text about-me__header">Студент</div>
       <div className="about-me__line" />
 
       <div className="about-me__info">
+
         <div className="about-me__description">
-          <div className="about-me__name">Сергей</div>
-          <div className="about-me__personal-info">Фронтенд-разработчик, 46 лет</div>
-          <div className="about-me__biography">!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</div>
-          <button className="about-me__github-link">GitHub</button>
+          <div className="about-me__text about-me__name">Сергей</div>
+          <div className="about-me__text about-me__personal-info">Фронтенд-разработчик, 46 лет</div>
+          <div className="about-me__text about-me__biography">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin augue leo, condimentum ut congue quis, consequat quis mi. Quisque pretium vehicula quam quis scelerisque. Nunc condimentum condimentum aliquet. Aliquam erat volutpat. Sed laoreet sapien nec magna volutpat vehicula. Vestibulum aliquet tortor quis vehicula ullamcorper.</div>
+          <button className="about-me__text about-me__github-link">GitHub</button>
         </div>
+
         <div className="about-me__photo"></div>
       </div>
     </section>
