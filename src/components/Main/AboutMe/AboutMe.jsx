0ea82e227a8d31fa1f700343b33fc5d2e60627ed
Sergey Grigorash <adviser@bk.ru>
2023-11-03 13:55:05 +0500

    feat: make about-me__info

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AboutMe.css";
+import profilePhoto from "../../../images/profilePhoto.jpg";
 
 const AboutMe = () => {
   return (
@@ -8,15 +9,29 @@ const AboutMe = () => {
       <div className="about-me__line" />
 
       <div className="about-me__info">
-
         <div className="about-me__description">
           <div className="about-me__text about-me__name">Сергей</div>
-          <div className="about-me__text about-me__personal-info">Фронтенд-разработчик, 46 лет</div>
-          <div className="about-me__text about-me__biography">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin augue leo, condimentum ut congue quis, consequat quis mi. Quisque pretium vehicula quam quis scelerisque. Nunc condimentum condimentum aliquet. Aliquam erat volutpat. Sed laoreet sapien nec magna volutpat vehicula. Vestibulum aliquet tortor quis vehicula ullamcorper.</div>
-          <button className="about-me__text about-me__github-link">GitHub</button>
-        </div>
+          <div className="about-me__text about-me__personal-info">
+            Фронтенд-разработчик, 46 лет
+          </div>
+          <div className="about-me__text about-me__biography">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin augue
+            leo, condimentum ut congue quis, consequat quis mi. Quisque pretium
+            vehicula quam quis scelerisque. Nunc condimentum condimentum
+            aliquet. Aliquam erat volutpat. Sed laoreet sapien nec magna
+            volutpat vehicula. Vestibulum aliquet tortor quis vehicula
+            ullamcorper.
+          </div>
 
-        <div className="about-me__photo"></div>
+          <button className="about-me__text about-me__github-link">
+            GitHub
+          </button>
+        </div>
+        <img
+          src={profilePhoto}
+          className="about-me__photo"
+          alt="Фото Сергея"
+        />
       </div>
     </section>
   );
