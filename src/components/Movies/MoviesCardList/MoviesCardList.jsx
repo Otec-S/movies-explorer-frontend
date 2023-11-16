932648353762cef045bcc72223083473e5a362b1
Sergey Grigorash <adviser@bk.ru>
2023-11-16 11:48:15 +0500

    feat: make MoviesCardList 1280

---
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./MoviesCardList.css";
+import MoviesCard from "../MoviesCard/MoviesCard";
+
+const MoviesCardList = () => {
+  return (
+    <ul className="movies-card-list">
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+      <li>
+        <MoviesCard />
+      </li>
+    </ul>
+  );
+};
+
+export default MoviesCardList;
