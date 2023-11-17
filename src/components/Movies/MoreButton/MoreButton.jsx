c5965a88910acd0f5febd3696bfa6a6ef1d98bf2
Sergey Grigorash <adviser@bk.ru>
2023-11-17 09:03:38 +0500

    feat: make Slider

--- src/components/Movies/MoreButton/MoreButton.jsx
+++ src/components/Movies/MoreButton/MoreButton.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./MoreButton.css";
-import { Link } from "react-router-dom";
 
 
 const MoreButton = () => {
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 import "./Slider.css";
 
 const Slider = () => {
   return (
-    <div>
-      
-    </div>
+    <label className ="switch">
+      <input type="checkbox" />
+      <span className="slider round"/>
+    </label>
   );
 };
 
