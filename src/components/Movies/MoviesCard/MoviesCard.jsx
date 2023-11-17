a75a280b34e70aaf19d16cfc00f5f1aca363cb75
Sergey Grigorash <adviser@bk.ru>
2023-11-17 13:27:28 +0500

    feat: make SaveCheckbox

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import "./MoviesCard.css";
+import SaveCheckbox from "../../SaveCheckbox/SaveCheckbox";
 
 const MoviesCard = () => {
   return (
     <div className="movies-card">
       <div className="movies-card__description">
         <div className="movies-card__description__text">
-          <h1 className="movies-card__description__title">33 слова о дизайне</h1>
+          <h1 className="movies-card__description__title">
+            33 слова о дизайне
+          </h1>
           <p className="movies-card__description__movie-duration">1ч 47м</p>
         </div>
-        <div className="movies-card__description__icon"></div>
+        <div className="movies-card__description__icon">
+          <SaveCheckbox />
+        </div>
       </div>
       <div className="movies-card__picture"></div>
     </div>
