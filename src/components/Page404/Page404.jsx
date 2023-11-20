92355c3d7634bfdd53a02e8b5cb53bcfb3bd8582
Sergey Grigorash <adviser@bk.ru>
2023-11-20 14:49:55 +0500

    feat: complete Page404

--- src/components/Page404/Page404.jsx
+++ src/components/Page404/Page404.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import "./Page404.css";
 
 const Page404 = () => {
   return (
-    <div>
-      
-    </div>
+    <main className='page404'>
+      <div className="page404__number">404</div>
+      <div className="page404__not-found">Страница не найдена</div>
+      <button className="page404__return-button">Назад</button>
+    </main>
   );
 };
 
