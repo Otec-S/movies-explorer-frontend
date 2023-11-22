54a4726e2eaee6e4c72c59043d8c4a8927335197
Sergey Grigorash <adviser@bk.ru>
2023-11-22 09:54:16 +0500

    fix: expanded Link for the About me block

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -17,7 +17,17 @@ const AboutMe = () => {
             Фронтенд-разработчик, 46 лет
           </p>
           <p className="about-me__biography">
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum vel itaque earum voluptatibus praesentium! Harum sapiente nemo officiis rerum in facere, veritatis alias eius sed minus quisquam provident mollitia explicabo reprehenderit adipisci maxime expedita optio quos ipsam porro nam at! Quia, fugit. Nostrum beatae, ex impedit quidem, dignissimos ab quos inventore cupiditate consectetur eius fugiat! Dicta maxime quasi sint magni nostrum animi laborum assumenda nihil incidunt. Error cupiditate facere, sed quia earum nemo unde eaque. At labore officiis fuga quasi? Corporis earum vel fugiat ducimus magni cum voluptatem! Assumenda quaerat veniam ab inventore. Reprehenderit pariatur est porro quia aspernatur labore.
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum vel
+            itaque earum voluptatibus praesentium! Harum sapiente nemo officiis
+            rerum in facere, veritatis alias eius sed minus quisquam provident
+            mollitia explicabo reprehenderit adipisci maxime expedita optio quos
+            ipsam porro nam at! Quia, fugit. Nostrum beatae, ex impedit quidem,
+            dignissimos ab quos inventore cupiditate consectetur eius fugiat!
+            Dicta maxime quasi sint magni nostrum animi laborum assumenda nihil
+            incidunt. Error cupiditate facere, sed quia earum nemo unde eaque.
+            At labore officiis fuga quasi? Corporis earum vel fugiat ducimus
+            magni cum voluptatem! Assumenda quaerat veniam ab inventore.
+            Reprehenderit pariatur est porro quia aspernatur labore.
           </p>
           <Link
             className="about-me__github-link"
@@ -33,50 +43,56 @@ Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum vel itaque earum
       <div className="about-me__portfolio">
         <h3 className="about-me__portfolio-title">Портфолио</h3>
 
-        <div className="about-me__portfolio-site">
-          <p className="about-me__portfolio-site_name">Статичный сайт</p>
-
-          <Link to="https://otec-s.github.io/russian-travel" target="_blank">
+        <Link
+          className="about-me__portfolio-site-link"
+          to="https://otec-s.github.io/russian-travel"
+          target="_blank"
+        >
+          <div className="about-me__portfolio-site">
+            <h4 className="about-me__portfolio-site-name">Статичный сайт</h4>
             <img
               src={aboutMeArrow}
               alt="Указатель на сайт"
-              className="about-me__portfolio-site_link"
+              className="about-me__portfolio-site-arrow"
             />
-          </Link>
-        </div>
+          </div>
+        </Link>
 
         <div className="about-me__line about-me__line_lightgrey" />
 
-        <div className="about-me__portfolio-site">
-          <p className="about-me__portfolio-site_name">Адаптивный сайт</p>
-
-          <Link to="https://www.adviser-spb.ru/" target="_blank">
+        <Link
+          className="about-me__portfolio-site-link"
+          to="https://www.adviser-spb.ru"
+          target="_blank"
+        >
+          <div className="about-me__portfolio-site">
+            <h4 className="about-me__portfolio-site-name">Адаптивный сайт</h4>
             <img
               src={aboutMeArrow}
               alt="Указатель на сайт"
-              className="about-me__portfolio-site_link"
+              className="about-me__portfolio-site-arrow"
             />
-          </Link>
-        </div>
+          </div>
+        </Link>
 
         <div className="about-me__line about-me__line_lightgrey" />
 
-        <div className="about-me__portfolio-site">
-          <p className="about-me__portfolio-site_name">
-            Одностраничное приложение
-          </p>
-
-          <Link
-            to="https://otec-s.students.nomoredomainsrocks.ru"
-            target="_blank"
-          >
+        <Link
+          className="about-me__portfolio-site-link"
+          to="https://otec-s.students.nomoredomainsrocks.ru"
+          target="_blank"
+        >
+          <div className="about-me__portfolio-site">
+            <h4 className="about-me__portfolio-site-name">
+              Одностраничное приложение
+            </h4>
             <img
               src={aboutMeArrow}
               alt="Указатель на сайт"
-              className="about-me__portfolio-site_link"
+              className="about-me__portfolio-site-arrow"
             />
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
     </section>
   );
