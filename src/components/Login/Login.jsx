aa6fb9c6942212d945c8a783f3e4d755a12d3c79
Sergey Grigorash <adviser@bk.ru>
2023-12-11 09:18:18 +0500

    feat: начал делать логирование

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import WelcomeForm from "../WelcomeForm/WelcomeForm";
+import { useNavigate } from "react-router-dom";
+import * as auth from "../../utils/MainApi";
 
-const Login = () => {
+export default function Login() {
+  //стейт для формы логирования
+  const [formValue, setFormValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  }
+
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  
+    try {
+      if (!formValue.email || !formValue.password) {
+        return;
+      }
+  
+      const data = await auth.authorize(formValue.email, formValue.password);
+  
+      if (data.token) {
+        setFormValue({ email: "", password: "" });
+        // Используем колбэк для гарантии актуальности данных
+        setFormValue(() => {
+          navigate("/", { replace: true });
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      // Обработка ошибок, например, уведомление пользователя
+    }
+  }
+  
+
   return (
     <WelcomeForm
       greetings="Рады видеть!"
@@ -12,6 +56,4 @@ const Login = () => {
       welcomeLink="/signup"
     />
   );
-};
-
-export default Login;
+}
--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,72 @@
 import { SAVED_MOVIES_BASE } from "../constants.js";
 
+//авторизация
+// export function authorize(email, password) {
+//   return fetch(`${SAVED_MOVIES_BASE}/signin`, {
+//     method: "POST",
+//     credentials: 'include', // теперь куки посылаются вместе с запросом
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ email, password }),
+//   })
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       if (data.token) {
+//         localStorage.setItem("token", data.token);
+//         return data; //возвращается только токен
+//       }
+//     })
+//     .catch(console.error);
+// }
+
+// Вспомогательная функция для извлечения токена из куки
+function getTokenFromCookie(cookie) {
+  const tokenMatch = cookie.match(/jwt=([^;]*)/);
+  return tokenMatch ? tokenMatch[1] : null;
+}
+
+//авторизация
+export async function authorize(email, password) {
+  try {
+    const response = await fetch(`${SAVED_MOVIES_BASE}/signin`, {
+      method: "POST",
+      credentials: 'include',
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      // Получаем токен из куки
+      const token = getTokenFromCookie(response.headers.get('Set-Cookie'));
+
+      // Далее можно использовать токен по необходимости
+      console.log('Token:', token);
+
+      const data = await response.json();
+
+      if (data.token) {
+        // Ваши дополнительные действия с токеном
+      }
+
+      return data;
+    } else {
+      // Обработка ошибки, если response.ok === false
+      console.error(`Server returned an error: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error(error);
+    // Обработка ошибки, например, throw error; или другие действия
+  }
+}
+
+
 //получение с бэка всего списка сохраненных фильмов (убрали из скобок токен)
 export const getAllSavedMovies = async () => {
   try {
