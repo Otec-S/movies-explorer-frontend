07a884a72880a73f64b66b289ebcd80f6b560450
Sergey Grigorash <adviser@bk.ru>
2023-12-26 11:50:27 +0500

    feat: при отсутствии найденных сохраненных фильмов вставил мемчик

--- src/components/SavedMoviesCardList/SavedMoviesCardList.jsx
+++ src/components/SavedMoviesCardList/SavedMoviesCardList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./SavedMoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
+import nothingFound from "../../images/nothing-found.jpg";
 
 const SavedMoviesCardList = ({
   isSaved,
@@ -13,7 +14,7 @@ const SavedMoviesCardList = ({
   savedMovieSearchQuery,
   setSavedMovieSearchQuery,
   isSearching,
-  setIsSearching
+  setIsSearching,
 }) => {
   console.log("savedMovieSearchQuery", savedMovieSearchQuery);
   console.log(
@@ -24,9 +25,8 @@ const SavedMoviesCardList = ({
   // Проверка на наличие поискового запроса и наличие фильмов
   const displayMovies =
     //поисковая строка заполнена ИЛИ есть найденные фильмы => показываем массив найденных
-    isSearching && (
-      savedMovieSearchQuery !== "" ||
-       filteredSavedMoviesArray.length > 0)
+    isSearching &&
+    (savedMovieSearchQuery !== "" || filteredSavedMoviesArray.length > 0)
       ? filteredSavedMoviesArray
       : //иначе показываем массив всех фильмов
         allSavedMovies;
@@ -39,33 +39,42 @@ const SavedMoviesCardList = ({
 
   return (
     <>
-      {/* Секция отрисовки карточек */}
-      <section className="movies-card-section">
-        <ul className="movies-card-list">
-          {displayMovies?.map((item) => {
-            return (
-              <MoviesCard
-                key={item.movieId}
-                isSaved={isSaved}
-                nameRU={item.nameRU}
-                duration={item.duration}
-                image={item.image}
-                trailerLink={item.trailerLink}
-                country={item.country}
-                director={item.director}
-                year={item.year}
-                description={item.description}
-                thumbnail={item.thumbnail}
-                movieId={item.movieId}
-                owner={item.owner}
-                nameEN={item.nameEN}
-                handleSaveStatusChange={handleSaveStatusChange}
-                allSavedMovies={allSavedMovies}
-              />
-            );
-          })}
-        </ul>
-      </section>
+      {displayMovies.length > 0 ? (
+        <section className="movies-card-section">
+          <ul className="movies-card-list">
+            {displayMovies?.map((item) => {
+              return (
+                <MoviesCard
+                  key={item.movieId}
+                  isSaved={isSaved}
+                  nameRU={item.nameRU}
+                  duration={item.duration}
+                  image={item.image}
+                  trailerLink={item.trailerLink}
+                  country={item.country}
+                  director={item.director}
+                  year={item.year}
+                  description={item.description}
+                  thumbnail={item.thumbnail}
+                  movieId={item.movieId}
+                  owner={item.owner}
+                  nameEN={item.nameEN}
+                  handleSaveStatusChange={handleSaveStatusChange}
+                  allSavedMovies={allSavedMovies}
+                />
+              );
+            })}
+          </ul>
+        </section>
+      ) : (
+        <section className="nothing-found-image-block">
+          <img
+            src={nothingFound}
+            alt="Ничего не найдено"
+            className="nothing-found-image"
+          />
+        </section>
+      )}
     </>
   );
 };
